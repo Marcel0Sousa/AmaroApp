1d63be51de1b68b7fd77dadb9e857cc1074ffe0a
Marcel0Sousa <tchelospy@gmail.com>
2020-12-13 08:38:22 -0300

    Add Activity de detalhes do produto

---
+++ app/src/main/java/co/marcelosousa/amaro/DetalhesActivity.java
@@ -0,0 +1,14 @@
+package co.marcelosousa.amaro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetalhesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhes);
+    }
+}
